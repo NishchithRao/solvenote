b7eef69d1fe7fe4bf765cb02aba4718ab8150673
Nishchith Rao <devmonk4@gmail.com>
2021-04-23 01:13:53 +0530

    format: adjust spacing

--- src/components/main/Solution.vue
+++ src/components/main/Solution.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card max-width="100%" height="100vh" flat class="rounded-0 text-h5 grey lighten-3 px-3 pt-6 text-left ">
+    <v-card max-width="100%" height="100vh" flat class="rounded-0 text-h5 grey lighten-3 px-3 sm-pt-6 pt-2 text-left ">
         <span v-if="getScreenValue">{{getSolution}}</span>
     </v-card>
 </template>
