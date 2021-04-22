fd50011cf53bb3a6e9c5624473a567b1738b877d
Nishchith Rao <devmonk4@gmail.com>
2021-04-23 01:13:04 +0530

    format: change operators to unicode and add color to operators

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
+import { typographicMathSymbols } from "./methods";
+
 export const highlightNumbers = (value) =>
   value.replace(/(\d+)/g, (num) => `<span class="number">${num}</span>`);
 
@@ -8,20 +10,36 @@ export const extractText = (value) => {
 };
 
 export const highlightHeading = (value) => {
-    value = value.replaceAll(/\s+/gm,'randomText321');
+  value = value.replaceAll(/\s+/gm, "randomText321");
   value = value.replace(
-    /#\w+/iygm,
-    (text) => `<span class="heading">${text}</span>`);
-    return value.replaceAll(/randomText321/gm,' ');
-};
-export const highlightOperators = (value) => {
-    return value.replace(/[+/\-*%]+/gm,num => `<span class="operator">${num}</span>`);
+    /#\w+/gimy,
+    (text) => `<span class="heading">${text}</span>`
+  );
+  return value.replaceAll(/randomText321/gm, " ");
 };
 
-export const processString = value => {
-  value = value.replaceAll(/[a-z=]/gmi,"");
-  value =value.replaceAll(/(?<=(\d(.)\d)|\d)(?=[^\d(.)\d\s])|(?<=[^\d(.)\d\s])(?=\d)/gm," ");
-  value = value.replaceAll('(',' ( ');
-  value = value.replaceAll(')',' ) ');
+export const changeOperators = (value) => {
+  Object.entries(typographicMathSymbols).map(
+    (arr) =>
+      (value = value.replaceAll(
+        arr[1],
+        (operator) => `<span class="operator">${operator}</span>`
+      ))
+  );
   return value;
-}
+};
+export const processString = (value) => {
+  value=value.replaceAll(/per/gm,'*');
+  value=value.replaceAll(/among/gm,'/');
+  value = value.replaceAll(/[a-z=]/gim, "");
+  Object.entries(typographicMathSymbols).map(
+    (arr) => (value = value.replaceAll(arr[1], arr[0]))
+  );
+  value = value.replaceAll(
+    /(?<=(\d(.)\d)|\d)(?=[^\d(.)\d\s])|(?<=[^\d(.)\d\s])(?=\d)/gm,
+    " "
+  );
+  value = value.replaceAll("(", " ( ");
+  value = value.replaceAll(")", " ) ");
+  return value;
+};
